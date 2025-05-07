f1a0b4d95f80d1c86ec4075ed7702bb653103400
santhosh <saisanthosh934@gmail.com>
2025-05-07 20:20:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My React App</h1>
+        <p>
+          This application is deployed via CI/CD pipeline
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
